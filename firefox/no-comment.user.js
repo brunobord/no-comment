c2f2d913cc5e2f209a69f848b18c77ae1f34c79b
Bruno Bord <bruno@jehaisleprintemps.net>
2012-07-18 22:52:29 +0200

    nouvelobs, adieu. fixes #10

--- firefox/no-comment.user.js
+++ firefox/no-comment.user.js
@@ -4,6 +4,7 @@
 // @version 0.2
 // @match http://*.sudouest.fr/*
 // @match http://*.humanite.fr/*
+// @match http://*.nouvelobs.fr/*
 // @match http://*.ladepeche.fr/*
 // @match http://*.lesechos.fr/*
 // @match http://fr.news.yahoo.com/*
@@ -12,7 +13,8 @@
 // ==/UserScript==
 
 GM_addStyle("@-moz-document domain(sudouest.fr) {     /* Remove comments from sudouest */ #comments {     display: none !important; }  } ");
-GM_addStyle("@-moz-document domain(humanite.fr) {     /* Remove comments from humanite */ #comments, #comment_box {     display: none !important; }  } ");
+GM_addStyle("@-moz-document domain(humanite.fr) {     /* Remove comments from humanite */ #comments, #comment_box, .comment_sep_number {     display: none !important; }  } ");
+GM_addStyle("@-moz-document domain(nouvelobs.fr) {     /* Remove comments from nouvelobs */ #reactions {     display: none !important; }  } ");
 GM_addStyle("@-moz-document domain(ladepeche.fr) {     /* Remove comments from ladepeche */ #commentaires {     display: none !important; }  } ");
 GM_addStyle("@-moz-document domain(lesechos.fr) {     /* Remove comments from lesechos */ .commentlist {     display: none !important; }  } ");
 GM_addStyle("@-moz-document domain(fr.news.yahoo.com) {     /* Remove comments from yahoo */ .yom-comments, #yom-comments {     display: none !important; }  } ");
