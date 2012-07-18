cb6954fce25f844f0e37b45dc0ee28054077d6de
Bruno Bord <bruno@jehaisleprintemps.net>
2012-07-18 19:47:00 +0200

    zapped humanite.fr. refs #9

--- firefox/no-comment.user.js
+++ firefox/no-comment.user.js
@@ -3,6 +3,7 @@
 // @description Remove comments on news websites
 // @version 0.2
 // @match http://*.sudouest.fr/*
+// @match http://*.humanite.fr/*
 // @match http://*.ladepeche.fr/*
 // @match http://*.lesechos.fr/*
 // @match http://fr.news.yahoo.com/*
@@ -11,6 +12,7 @@
 // ==/UserScript==
 
 GM_addStyle("@-moz-document domain(sudouest.fr) {     /* Remove comments from sudouest */ #comments {     display: none; }  } ");
+GM_addStyle("@-moz-document domain(humanite.fr) {     /* Remove comments from humanite */ #comment_box {     display: none; }  } ");
 GM_addStyle("@-moz-document domain(ladepeche.fr) {     /* Remove comments from ladepeche */ #commentaires {     display: none; }  } ");
 GM_addStyle("@-moz-document domain(lesechos.fr) {     /* Remove comments from lesechos */ .commentlist {     display: none; }  } ");
 GM_addStyle("@-moz-document domain(fr.news.yahoo.com) {     /* Remove comments from yahoo */ .yom-comments, #yom-comments {     display: none; }  } ");
