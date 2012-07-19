1c3f239cadee7e0eef942790d579052af76073fd
Bruno Bord <bruno@jehaisleprintemps.net>
2012-07-19 21:42:37 +0200

    must have reset by mistake. here we go

---
+++ firefox/nocomment.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name No Comment
+// @description Remove comments on news websites
+// @version 0.1
+// @match http://*.sudouest.fr/*
+// ==/UserScript==
+
+GM_addStyle("#comments { display: none; }");
